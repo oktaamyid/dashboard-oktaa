0f60edf121319c120c7154c52232a3a5cf0b91be
Tayen15 <tiantiantitik@gmail.com>
2025-03-27 19:32:56 +0700

    Remove RootLayout and metadata files to streamline the dashboard structure

--- src/app/metadata.ts
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-     metadataBase: new URL("https://firtiansyah.my.id"),
-     title: "Oktaa.",
-     icons: "/oktaa-white.svg",
-     authors: [{ name: "Firtiansyah Okta Resama", url: "https://your-domain.com"}],
-     description: "Oktaa is the domain name from Firtiansyah Okta, and then use the Okta for brand identity name."
-}
