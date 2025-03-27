3f1104613aa86a762546ce2f1051cbd1c588f7d5
Tayen15 <tiantiantitik@gmail.com>
2025-03-27 19:33:19 +0700

    Refactor dashboard layout to include Sidebar and Header components, enhancing structure and responsiveness

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,34 @@
-import React from "react";
-import RootLayout from "./rootLayout";
+"use client";
 
-export default function MainLayout({children}: { children: React.ReactNode }) {
-     <RootLayout>{children}</RootLayout>
+import Sidebar from "@/components/sidebar";
+import Header from "@/components/header";
+import { useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
+import { usePathname } from "next/navigation";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+     const [isOpen, setIsOpen] = useState(true);
+     const pathname = usePathname();
+
+     const isDashboard = pathname.startsWith("/dashboard");
+
+     return (
+          <>
+               <Analytics />
+               {isDashboard ? (
+                    <div className="antialiased bg-gradient-to-br from-gray-700 to-black/80">
+                         <div className="flex min-h-screen">
+                              <Sidebar isOpen={isOpen} />
+                              <div className="flex-1">
+                                   <Header isOpen={isOpen} onToggleSidebar={() => setIsOpen(!isOpen)} />
+                                   <div className="p-6">{children}</div>
+                              </div>
+                         </div>
+                    </div>
+               ) : (
+                    // Layout default untuk halaman lain
+                    <div className="p-6">{children}</div>
+               )}
+          </>
+     );
 }
