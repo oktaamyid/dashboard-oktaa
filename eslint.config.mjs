03de3affb8fe3645c986c4de2cbe7ca556f8048e
Tayen15 <tiantiantitik@gmail.com>
2025-07-10 23:14:27 +0700

    feat: add custom ESLint rules for unused variables in TypeScript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn", 
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_"
+        }
+      ]
+    }
+  })
 ];
 
 export default eslintConfig;
